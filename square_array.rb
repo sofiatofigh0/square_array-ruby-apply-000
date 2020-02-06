e9d04f855ce6052bfeb7e6023b6ff39573a8adf8
sofia tofigh <sst2140@columbia.edu>
2020-02-06 22:04:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
+my_array=[1,2,3]
+
+def square_array(my_array)
+  
+new_array=[]
+counter=0
+
+while counter<my_array.length do|numbers|
+    puts "#{numbers**2}"
+    
+  end
+
+
 end
+
