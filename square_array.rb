89425c236fe7a4706ef6b8b654524d55a8b579bd
sofia tofigh <sst2140@columbia.edu>
2020-02-06 22:15:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,13 @@
 my_array=[1,2,3]
-c=0
 
 def square_array(numbers)
   
 my_array.each do |numbers|
-new_array << my_array[counter]**2
+new_array << my_array(numbers)**2
+
+end
+
+return new_array
 
 end
 
