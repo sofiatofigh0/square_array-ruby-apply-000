46b43361c445ff368a5796c6a9e3e6021b1ec208
sofia tofigh <sst2140@columbia.edu>
2020-02-06 22:08:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,8 +5,8 @@ def square_array(my_array)
 new_array=[]
 counter=0
 
-while array[counter] do
-  new_array << array[counter] ** 2
+while my_array[counter] do
+  new_array << my_array[counter] ** 2
   counter+=1
     
   end
